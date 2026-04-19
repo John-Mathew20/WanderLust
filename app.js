306dea3b9cba0867173c0da88d2af279fcc5a323
John Mathew14 <JohnMathew141105@gmail.com>
2026-04-19 23:21:17 +0530

    files were added

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const port = 9000;
+const url = "mongodb://127.0.0.1:27017/test";
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "/views")));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+main()
+  .then(() => {
+    console.log(`Connected on ${url}`);
+  })
+  .catch((err) => {
+    throw err;
+  });
+
+async function main() {
+  await mongoose.connect(url);
+}
+
+//home route
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello ${port} working</h1>`);
+});
+
+app.listen(port, () => {
+  console.log(`Running on ${port}`);
+});
