6b938b4600ce9564901aaa46e64bc163ba558078
John Mathew14 <JohnMathew141105@gmail.com>
2026-05-09 03:03:46 +0530

    Files were added and modified

--- routes/listings.js
+++ routes/listings.js
@@ -9,22 +9,28 @@ const { isLoggedIn, isOwner, validateListing } = require("../utils/middleware");
 
 const listingController = require("../controllers/listing");
 
-//index route
-router.get("/", wrapAsync(listingController.index));
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  .post(
+    isLoggedIn,
+    validateListing,
+    wrapAsync(listingController.createNewListing),
+  );
+
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    validateListing,
+    wrapAsync(listingController.updateListing),
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
 //new route
 router.get("/new", isLoggedIn, wrapAsync(listingController.renderNewForm));
-
-// show route
-router.get("/:id", wrapAsync(listingController.showListing));
-
-//create route
-router.post(
-  "/",
-  isLoggedIn,
-  validateListing,
-  wrapAsync(listingController.createNewListing),
-);
 
 //edit route
 router.get(
@@ -32,23 +38,6 @@ router.get(
   isLoggedIn,
   isOwner,
   wrapAsync(listingController.renderEditForm),
-);
-
-// update route
-router.put(
-  "/:id",
-  isLoggedIn,
-  isOwner,
-  validateListing,
-  wrapAsync(listingController.updateListing),
-);
-
-//delete route
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isOwner,
-  wrapAsync(listingController.destroyListing),
 );
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -7,21 +7,22 @@ const passport = require("passport");
 const { saveRedirectUrl } = require("../utils/middleware");
 const userController = require("../controllers/users");
 
-router.get("/signup", userController.renderSignForm);
+router
+  .route("/signup")
+  .get(userController.renderSignForm)
+  .post(wrapAsync(userController.signUp));
 
-router.post("/signup", wrapAsync(userController.signUp));
-
-router.get("/login", userController.renderLoginForm);
-
-router.post(
-  "/login",
-  saveRedirectUrl,
-  passport.authenticate("local", {
-    failureRedirect: "/login",
-    failureFlash: true,
-  }),
-  userController.login,
-);
+router
+  .route("/login")
+  .get(userController.renderLoginForm)
+  .post(
+    saveRedirectUrl,
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true,
+    }),
+    userController.login,
+  );
 
 router.get("/logout", userController.logout);
 
